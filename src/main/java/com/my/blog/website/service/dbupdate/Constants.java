b08ccbe24bca004809b2e0913d077887d5f5122d
苏登辉 <dhsu2@iflytek.com>
2019-06-28 14:07:48 +0800

    修改数据库配置信息读取方式

--- src/main/java/com/my/blog/website/service/dbupdate/Constants.java
+++ src/main/java/com/my/blog/website/service/dbupdate/Constants.java
@@ -1,43 +1,104 @@
 package com.my.blog.website.service.dbupdate;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.ibatis.io.Resources;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * 常量
  */
+@Component
+@ConfigurationProperties(prefix = "db.update")
 public class Constants {
-	public static String subject;//邮件主题
-	public static String sqlFile;//读取邮件的sql内容后保存到的路径，如E://tale.sql
-	public static String readEmail;//要读取的qq邮箱,如348673242@qq.com
-	public static String sendEmail;//发件人qq邮箱地址,如348673242@qq.com
-	public static String sendName;//发件人姓名
-	public static String authpwd;//授权码
-	public static String mysqlAddr;//本地mysql安装目录，E:\mysql-5.6.24-win32
-	public static String db_url;//jdbc:mysql://localhost:3306/tale?characterEncoding=UTF-8
-	public static String db_username;//用户名，root
-	public static String db_pwd;//密码，admin
-	
-	
-	
-	static {
-		 
-		try {
-			Properties props = Resources.getResourceAsProperties("updateKnowledgeBag.properties");
-			subject = props.getProperty("subject"); 
-			sqlFile = props.getProperty("sqlFile"); 
-			readEmail = props.getProperty("readEmail"); 
-			sendEmail = props.getProperty("sendEmail"); 
-			sendName = props.getProperty("sendName"); 
-			authpwd = props.getProperty("authpwd"); 
-			mysqlAddr = props.getProperty("mysqlAddr"); 
-			db_url = props.getProperty("db_url"); 
-			db_username = props.getProperty("db_username"); 
-			db_pwd = props.getProperty("db_pwd"); 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
+
+	public static String subject;// 邮件主题
+	public static String sqlFile;// 读取邮件的sql内容后保存到的路径，如E://tale.sql
+	public static String readEmail;// 要读取的qq邮箱,如348673242@qq.com
+	public static String sendEmail;// 发件人qq邮箱地址,如348673242@qq.com
+	public static String sendName;// 发件人姓名
+	public static String authpwd;// 授权码
+	public static String mysqlAddr;// 本地mysql安装目录，E:\mysql-5.6.24-win32
+	public static String db_url;// jdbc:mysql://localhost:3306/tale?characterEncoding=UTF-8
+	public static String db_username;// 用户名，root
+	public static String db_pwd;// 密码，admin
+
+	public String getSubject() {
+		return subject;
 	}
+
+	public void setSubject(String subject) {
+		Constants.subject = subject;
+	}
+
+	public String getSqlFile() {
+		return sqlFile;
+	}
+
+	public void setSqlFile(String sqlFile) {
+		Constants.sqlFile = sqlFile;
+	}
+
+	public String getReadEmail() {
+		return readEmail;
+	}
+
+	public void setReadEmail(String readEmail) {
+		Constants.readEmail = readEmail;
+	}
+
+	public String getSendEmail() {
+		return sendEmail;
+	}
+
+	public void setSendEmail(String sendEmail) {
+		Constants.sendEmail = sendEmail;
+	}
+
+	public String getSendName() {
+		return sendName;
+	}
+
+	public void setSendName(String sendName) {
+		Constants.sendName = sendName;
+	}
+
+	public String getAuthpwd() {
+		return authpwd;
+	}
+
+	public void setAuthpwd(String authpwd) {
+		Constants.authpwd = authpwd;
+	}
+
+	public String getMysqlAddr() {
+		return mysqlAddr;
+	}
+
+	public void setMysqlAddr(String mysqlAddr) {
+		Constants.mysqlAddr = mysqlAddr;
+	}
+
+	public String getDb_url() {
+		return db_url;
+	}
+
+	public void setDb_url(String db_url) {
+		Constants.db_url = db_url;
+	}
+
+	public String getDb_username() {
+		return db_username;
+	}
+
+	public void setDb_username(String db_username) {
+		Constants.db_username = db_username;
+	}
+
+	public String getDb_pwd() {
+		return db_pwd;
+	}
+
+	public void setDb_pwd(String db_pwd) {
+		Constants.db_pwd = db_pwd;
+	}
+
 }
